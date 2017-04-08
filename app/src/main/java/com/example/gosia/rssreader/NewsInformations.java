e817d5fb5eb7b74ccf17eca5ae22adb65bc85a13
GosiaZ <s11483@pjwstk.edu.pl>
2017-04-08 15:54:13 +0100

    fixes

--- app/src/main/java/com/example/gosia/rssreader/NewsInformations.java
+++ app/src/main/java/com/example/gosia/rssreader/NewsInformations.java
@@ -1,15 +1,11 @@
 package com.example.gosia.rssreader;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * Created by Gosia on 25.03.2017.
